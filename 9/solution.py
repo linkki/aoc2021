7fe013868f88217d88fdab01080959c306c2f4a9
Virpi <1564285+viipu@users.noreply.github.com>
2021-12-03 22:12:23 +0200

    templates for solutions

---
+++ 9/solution.py
@@ -0,0 +1,23 @@
+# Advent of Code Day 9
+# Science Lab Linkki, Virpi Sumu
+
+def read_file(filename):
+    contents = []
+    with open(filename) as file:
+        for line in file:
+            contents.append((line.strip()))
+
+    return contents
+
+file_contents = read_file("input.txt")
+
+result1 = 0
+
+# Task 1: 
+print("Task 1:", result1)
+
+
+result2 = 0
+
+# Task 2: 
+print("Task 2:", result2)
