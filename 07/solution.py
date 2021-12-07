2ebab969c6b98a71e7610eaab1d4ca28a04ccd5e
Virpi <1564285+viipu@users.noreply.github.com>
2021-12-07 23:34:57 +0200

    day 7

--- 07/solution.py
+++ 07/solution.py
@@ -5,19 +5,29 @@ def read_file(filename):
     contents = []
     with open(filename) as file:
         for line in file:
-            contents.append((line.strip()))
+            contents = [int(number) for number in line.split(',')]
 
     return contents
 
 file_contents = read_file("input.txt")
 
-result1 = 0
+from functools import reduce
+
+result1 = []
+minimum = min(file_contents)
+maximum = max(file_contents)
+
+for number in range(minimum, maximum):
+    result1.append(reduce(lambda sum, item: sum + abs(number-item), file_contents, 0))
 
 # Task 1: 
-print("Task 1:", result1)
+print("Task 1:", min(result1))
 
+result2 = []
+sums = [sum(range(0, number+1)) for number in range(minimum, maximum+1)]
 
-result2 = 0
+for number in range(minimum, maximum):
+    result2.append(reduce(lambda sum, item: sum + sums[abs(number-item)], file_contents, 0))
 
 # Task 2: 
-print("Task 2:", result2)
+print("Task 2:", min(result2))
