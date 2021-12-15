0b1a1f37b6ff832e1c281eabffcf8661b95ac41a
Virpi <1564285+viipu@users.noreply.github.com>
2021-12-15 19:09:20 +0200

    day 14 part 2

--- 14/solution.py
+++ 14/solution.py
@@ -2,42 +2,77 @@
 # Science Lab Linkki, Virpi Sumu
 
 def read_file(filename):
-    template = []
+    chars = {}
+    template = {}
     rules = {}
     with open(filename) as file:
         for line in file:
             parts = line.strip().split(" -> ")
             if len(parts) == 1:
-                for c in parts[0]:
-                    template.append(c)
+                for i in range(len(parts[0])):
+                    if i < len(parts[0])-1:
+                        key = "".join([parts[0][i], parts[0][i+1]])
+                        if key not in template.keys():
+                            template[key] = 0
+                        template[key] += 1
+                        
             elif len(parts) == 2:
-                rules[parts[0]] = parts[1]
+                chars[parts[0][0]] = 0
+                chars[parts[0][1]] = 0
+                if parts[0] not in template.keys():
+                    template[parts[0]] = 0
+                beg = parts[0][0]
+                end = parts[0][1]
+                rules[parts[0]] = [beg + parts[1], parts[1] + end]
 
-    return (template, rules)
+    return (chars, template, rules)
 
-template, rules = read_file("input.txt")
-
+chars, template, rules = read_file("input.txt")
 from collections import Counter
+#print(template, "\n\n", rules, "\n\n", chars)
 
-for i in range(10):
-    counter = 0
-    for n in range(len(template) - 1):
-        key = "".join(template[n+counter:n+counter+2])
-        if key in rules:
-            template.insert(n+1+counter, rules[key])
-            counter += 1
-    print(len(template), Counter(template))
+for i in range(40):
+    new_template = {}
+    for key in template.keys():
+        new_template[key] = 0
+    for pair in template:
+        if template[pair] > 0:
+            original = template[pair]
+            for result in rules[pair]:
+                new_template[result] += original
+    template = new_template
+    if i == 9:
+        first_template = new_template
 
+for pair in first_template.keys():
+    chars[pair[0]] += first_template[pair]
+    chars[pair[1]] += first_template[pair]
 
-c = Counter(template)
+for char in chars.keys():
+    if chars[char] %2 == 0:
+        chars[char] //= 2
+    else:
+        chars[char] = (chars[char] + 1) //2
 
-result1 = c.most_common()[0][1] - c.most_common()[len(c)-1][1]
+result1 = max(chars.values()) - min(chars.values())
+
+for char in chars.keys():
+    chars[char] = 0
+
+for pair in template.keys():
+    chars[pair[0]] += template[pair]
+    chars[pair[1]] += template[pair]
+
+for char in chars.keys():
+    if chars[char] %2 == 0:
+        chars[char] //= 2
+    else:
+        chars[char] = (chars[char] + 1) //2
+
+result2 = max(chars.values()) - min(chars.values())
 
 # Task 1: 2068
 print("Task 1:", result1)
-
-
-result2 = 0
 
 # Task 2: 
 print("Task 2:", result2)
